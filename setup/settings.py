0d5a856a5de169d36ebcd4204a110da98d80df87
Laís Urano <95968249+uranolais@users.noreply.github.com>
2024-03-26 12:01:49 -0300

    adicionando validações nos models

--- setup/settings.py
+++ setup/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'escola',
     'rest_framework',
-    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -131,8 +130,5 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
-    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning'
+    ]
 }
