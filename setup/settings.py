fff9a241b1acc5f44d7f5ab143920e101b31047f
Laís Urano <95968249+uranolais@users.noreply.github.com>
2024-04-18 17:17:27 -0300

    curso_02_att

--- setup/settings.py
+++ setup/settings.py
@@ -40,13 +40,11 @@ INSTALLED_APPS = [
     'escola',
     'rest_framework',
     'django_filters',
-    'drf_yasg',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -136,5 +134,5 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
-    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning'
 }
